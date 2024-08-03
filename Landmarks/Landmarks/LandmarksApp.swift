7f9e1c5b04d09d01fb7e6c445519a2d34e94f47a
Ali Rahimi <ali.rahimi@gandom.co>
2024-08-03 16:04:53 +0330

    first page turtle rock page

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ali Rahimi on 03/08/2024.
+//
+
+import SwiftUI
+
+//entry point of application
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
